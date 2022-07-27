63155eda410ee717ce204bcc363076a3ed96c836
CLQuantizer <ezioxxk@gmail.com>
2022-07-27 22:33:18 +0100

    initial commit

---
+++ src/main/java/com/xiangcai/fry/FryApplication.java
@@ -0,0 +1,13 @@
+package com.xiangcai.fry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FryApplication.class, args);
+	}
+
+}
